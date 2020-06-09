2b42c76bf3cbc9d6427eb6a20ff05c4f1057aa34
Deen2544 <66002477+Deen2544@users.noreply.github.com>
2020-06-09 12:10:30 +0700

    Create Exercise4_Deen_S.py

---
+++ Exercise4_Deen_S.py
@@ -0,0 +1,9 @@
+FE = 55
+GB = 72.5
+ICS = 58.5
+CP = 84
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
